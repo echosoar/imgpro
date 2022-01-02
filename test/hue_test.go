1d30ec0a9d5900fd794e12b9c2cdbbe2f3c457cb
echosoar <82163514@qq.com>
2022-01-02 11:18:07 +0800

    test: hue png

--- test/hue_test.go
+++ test/hue_test.go
@@ -8,16 +8,20 @@ import (
 
 func TestHUEPng(t *testing.T) {
 	result := Run("./imgs/go.png", []string{"hue"})
-	if len(result["hue"].Rgba[0]) != 2 {
-		t.Fatal("hue error")
+	if len(result["hue"].Rgba[0]) != 3 {
+		t.Fatal("hue error", result["hue"].Rgba[0])
 	}
 
-	if result["hue"].Rgba[0][0].R != 255 || result["hue"].Rgba[0][0].G != 255 || result["hue"].Rgba[0][0].B != 255 || result["hue"].Rgba[0][0].A != 255 {
-		t.Fatal("hue error")
+	if result["hue"].Rgba[0][0].R != 106 || result["hue"].Rgba[0][0].G != 215 || result["hue"].Rgba[0][0].B != 229 || result["hue"].Rgba[0][0].A != 255 {
+		t.Fatal("hue 0 error")
 	}
 
-	if result["hue"].Rgba[0][1].R != 106 || result["hue"].Rgba[0][1].G != 215 || result["hue"].Rgba[0][1].B != 229 || result["hue"].Rgba[0][1].A != 255 {
-		t.Fatal("hue error")
+	if result["hue"].Rgba[0][1].R != 0 || result["hue"].Rgba[0][1].G != 0 || result["hue"].Rgba[0][1].B != 0 || result["hue"].Rgba[0][1].A != 255 {
+		t.Fatal("hue 1 error")
+	}
+
+	if result["hue"].Rgba[0][2].R != 246 || result["hue"].Rgba[0][2].G != 210 || result["hue"].Rgba[0][2].B != 162 || result["hue"].Rgba[0][1].A != 255 {
+		t.Fatal("hue 2 error")
 	}
 }
 
