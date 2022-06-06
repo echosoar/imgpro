7c341fb2cc183631c3eb595d863f32f711cf0c6b
Gao Yang <82163514@qq.com>
2022-06-06 13:46:30 +0000

    feat: mask

--- processor/qrcode.go
+++ processor/qrcode.go
@@ -98,8 +98,8 @@ type QRCodeItem struct {
 	version              int
 	size                 int
 	Pixels               []int
-	errorCorrectionLevel []int
-	mask                 []int
+	errorCorrectionLevel int
+	mask                 int
 }
 
 func (qr *QRCode) Run() {
@@ -107,7 +107,6 @@ func (qr *QRCode) Run() {
 	qr.binarization()
 	qr.findCorners()
 	qr.polymerizate()
-	qr.output()
 	fmt.Println("qr", len(qr.greyPixels))
 }
 
@@ -449,18 +448,6 @@ func (qr *QRCode) newCorner(lineRegionIndex int, lineRegion *QRCodeRegion, cente
 	corner.center = method.PerspectiveTransform(&corner.matrix, qrCornerSize/2, qrCornerSize/2)
 }
 
-func (qr *QRCode) output() {
-	rgba := make([]img.RGBA, len(qr.greyPixels))
-	for index, pixel := range qr.greyPixels {
-		if pixel <= 0 {
-			rgba[index] = method.ColorListToRGBA([]int{255, 255, 255, 255})
-		} else {
-			rgba[index] = method.ColorListToRGBA([]int{0, 0, 0, 255})
-		}
-	}
-	method.OutputToImg("./ignore_qrout.jpg", qr.Width, qr.Height, rgba)
-}
-
 // 聚合多个 corner，确认一张二维码
 // 一个 corner 有横向和纵向相对应的 corner的时候，才有可能是一个二维码
 func (qr *QRCode) polymerizate() {
@@ -558,13 +545,11 @@ func (qr *QRCode) polymerizateExec(centerCorner *QRCodeCorner, hCorner *QRCodeCo
 	qrCodeItem.matrix = matrix
 	qr.autoAdjustmentMatrix(qrCodeItem, &matrix)
 
-	rgba := make([]img.RGBA, qrCodeItem.size*qrCodeItem.size)
 	// 获取图像透视像素点
 	for line := 0; line < qrCodeItem.size; line++ {
 		for row := 0; row < qrCodeItem.size; row++ {
 			pixelIndex := line*qrCodeItem.size + row
 			qrCodeItem.Pixels[pixelIndex] = qrPixelWhite
-			rgba[pixelIndex] = img.RGBA{R: 255, G: 255, B: 255, A: 255}
 			// 之所以加0.5是为了获取到像素中心
 			point := method.PerspectiveTransform(&qrCodeItem.matrix, float64(row)+0.5, float64(line)+0.5)
 			// default isWhite
@@ -572,13 +557,11 @@ func (qr *QRCode) polymerizateExec(centerCorner *QRCodeCorner, hCorner *QRCodeCo
 				index := point.Y*qr.Width + point.X
 				if qr.greyPixels[index] != 0 {
 					qrCodeItem.Pixels[pixelIndex] = qrPixelBlack
-					rgba[pixelIndex] = img.RGBA{R: 0, G: 0, B: 0, A: 255}
 				}
 			}
 		}
 	}
 	qrCodeItem.decode()
-	method.OutputToImg("./ignore_target.jpg", qrCodeItem.size, qrCodeItem.size, rgba)
 }
 
 // 获取二维码版本
@@ -812,17 +795,21 @@ func (qrItem *QRCodeItem) getFormatData() error {
 	}
 	format = method.IntToBinary(newFormat, 15)
 
-	qrItem.errorCorrectionLevel = format[0:2]
-	qrItem.mask = format[2:5]
+	qrItem.errorCorrectionLevel = method.BinaryToInt(format[0:2])
+	qrItem.mask = method.BinaryToInt(format[2:5])
 	fmt.Println("qrItem", qrItem.errorCorrectionLevel, qrItem.mask)
 	return nil
 }
 
 func (qrItem *QRCodeItem) readData() error {
+	rgba := make([]img.RGBA, qrItem.size*qrItem.size)
+	fmt.Println("qrItem.size", qrItem.size)
 	// 从右向左，从低向上
 	y := qrItem.size - 1
 	x := qrItem.size - 1
 	step := -1
+	pixelCount := 0
+	parts := make([][]int, 0)
 	for x > 0 {
 		if x == 6 {
 			x--
@@ -830,10 +817,26 @@ func (qrItem *QRCodeItem) readData() error {
 
 		xs := []int{x, x - 1}
 		for _, xItem := range xs {
+			pixelIndex := y*qrItem.size + xItem
 			if qrItem.checkNotIsData(xItem, y) {
+				rgba[pixelIndex] = img.RGBA{R: 255, G: 0, B: 0, A: 255}
 				continue
 			}
-			qrItem.readBit(xItem, y)
+			partIndex := pixelCount / 8
+			if partIndex%2 == 0 {
+				rgba[pixelIndex] = img.RGBA{R: 0, G: 0, B: 0, A: 255}
+			} else {
+				rgba[pixelIndex] = img.RGBA{R: 255, G: 255, B: 255, A: 255}
+			}
+			pixelCount++
+			bit := qrItem.Pixels[pixelIndex]
+			if qrItem.dataMask(x, y) > 0 {
+				bit ^= 1
+			}
+			if len(parts) == partIndex {
+				parts = append(parts, []int{})
+			}
+			parts[partIndex] = append(parts[partIndex], bit)
 		}
 		y += step
 		if y < 0 || y >= qrItem.size {
@@ -842,6 +845,23 @@ func (qrItem *QRCodeItem) readData() error {
 			y += step
 		}
 	}
+
+	eccInfo := qrEccList[qrItem.version]
+	errLevel := 0
+	switch qrItem.errorCorrectionLevel {
+	case 1:
+		errLevel = 0
+	case 0:
+		errLevel = 1
+	case 3:
+		errLevel = 2
+	case 2:
+		errLevel = 3
+	}
+	eccItem := eccInfo[errLevel]
+	fmt.Println("parts", eccItem)
+
+	method.OutputToImg("./ignore_target.jpg", qrItem.size, qrItem.size, rgba)
 	return nil
 }
 
@@ -849,7 +869,17 @@ type QRDataInfo struct {
 	alignmentPatterns []int
 }
 
-var QRAlignmentPatterns = []QRDataInfo{
+// refs: https://www.thonky.com/qr-code-tutorial/error-correction-table
+type QRDataECC struct {
+	tdc     int // Total Number of Data Codewords for this Version and EC Level
+	ecc     int // EC Codewords Per Block
+	blocks1 int // Number of Blocks in Group 1
+	dc1     int // Number of Data Codewords in Each of Group 1's Blocks
+	blocks2 int // Number of Blocks in Group 2
+	dc2     int // Number of Data Codewords in Each of Group 2's Blocks
+}
+
+var QRDataInfoList = []QRDataInfo{
 	{},
 	{alignmentPatterns: []int{}},
 	{alignmentPatterns: []int{6, 18}},
@@ -893,6 +923,249 @@ var QRAlignmentPatterns = []QRDataInfo{
 	{alignmentPatterns: []int{6, 30, 58, 86, 114, 142, 170}},
 }
 
+var qrEccList = [][]QRDataECC{
+	{},
+	{ // version 1: L/M/Q/H 四个纠错级别，1/0/3/2
+		{19, 7, 1, 19, 0, 0},
+		{16, 10, 1, 16, 0, 0},
+		{13, 13, 1, 13, 0, 0},
+		{9, 17, 1, 9, 0, 0},
+	},
+	{ // version 2
+		{34, 10, 1, 34, 0, 0},
+		{28, 16, 1, 28, 0, 0},
+		{22, 22, 1, 22, 0, 0},
+		{16, 28, 1, 16, 0, 0},
+	},
+	{ // version 3
+		{55, 15, 1, 55, 0, 0},
+		{44, 26, 1, 44, 0, 0},
+		{34, 18, 2, 17, 0, 0},
+		{26, 22, 2, 13, 0, 0},
+	},
+	{ // version 4
+		{80, 20, 1, 80, 0, 0},
+		{64, 18, 2, 32, 0, 0},
+		{48, 26, 2, 24, 0, 0},
+		{36, 16, 4, 9, 0, 0},
+	},
+	{ // version 5
+		{108, 26, 1, 108, 0, 0},
+		{86, 24, 2, 43, 0, 0},
+		{62, 18, 2, 15, 2, 16},
+		{46, 22, 2, 11, 2, 12},
+	},
+	{ // version 6
+		{136, 18, 2, 68, 0, 0},
+		{108, 16, 4, 27, 0, 0},
+		{76, 24, 4, 19, 0, 0},
+		{60, 28, 4, 15, 0, 0},
+	},
+	{ // version 7
+		{156, 20, 2, 78, 0, 0},
+		{124, 18, 4, 31, 0, 0},
+		{88, 18, 2, 14, 4, 15},
+		{66, 26, 4, 13, 1, 14},
+	},
+	{ // version 8
+		{194, 24, 2, 97, 0, 0},
+		{154, 22, 2, 38, 2, 39},
+		{110, 22, 4, 18, 2, 19},
+		{86, 26, 4, 14, 2, 15},
+	},
+	{ // version 9
+		{232, 30, 2, 116, 0, 0},
+		{182, 22, 3, 36, 2, 37},
+		{132, 20, 4, 16, 4, 17},
+		{100, 24, 4, 12, 4, 13},
+	},
+	{ // version 10
+		{274, 18, 2, 68, 2, 69},
+		{216, 26, 4, 43, 1, 44},
+		{154, 24, 6, 19, 2, 20},
+		{122, 28, 6, 15, 2, 16},
+	},
+	{ // version 11
+		{324, 20, 4, 81, 0, 0},
+		{254, 30, 1, 50, 4, 51},
+		{180, 28, 4, 22, 4, 23},
+		{140, 24, 3, 12, 8, 13},
+	},
+	{ // version 12
+		{370, 24, 2, 92, 2, 93},
+		{290, 22, 6, 36, 2, 37},
+		{206, 26, 4, 20, 6, 21},
+		{158, 28, 7, 14, 4, 15},
+	},
+	{ // version 13
+		{428, 26, 4, 107, 0, 0},
+		{334, 22, 8, 37, 1, 38},
+		{244, 24, 8, 20, 4, 21},
+		{180, 22, 12, 11, 4, 12},
+	},
+	{ // version 14
+		{461, 30, 3, 115, 1, 116},
+		{365, 24, 4, 40, 5, 41},
+		{261, 20, 11, 16, 5, 17},
+		{197, 24, 11, 12, 5, 13},
+	},
+	{ // version 15
+		{523, 22, 5, 87, 1, 88},
+		{415, 24, 5, 41, 5, 42},
+		{295, 30, 5, 24, 7, 25},
+		{223, 24, 11, 12, 7, 13},
+	},
+	{ // version 16
+		{589, 24, 5, 98, 1, 99},
+		{453, 28, 7, 45, 3, 46},
+		{325, 24, 15, 19, 2, 20},
+		{253, 30, 3, 15, 13, 16},
+	},
+	{ // version 17
+		{647, 28, 1, 107, 5, 108},
+		{507, 28, 10, 46, 1, 47},
+		{367, 28, 1, 22, 15, 23},
+		{283, 28, 2, 14, 17, 15},
+	},
+	{ // version 18
+		{721, 30, 5, 120, 1, 121},
+		{563, 26, 9, 43, 4, 44},
+		{397, 28, 17, 22, 1, 23},
+		{313, 28, 2, 14, 19, 15},
+	},
+	{ // version 19
+		{795, 28, 3, 113, 4, 114},
+		{627, 26, 3, 44, 11, 45},
+		{445, 26, 17, 21, 4, 22},
+		{341, 26, 9, 13, 16, 14},
+	},
+	{ // version 20
+		{861, 28, 3, 107, 5, 108},
+		{669, 26, 3, 41, 13, 42},
+		{485, 30, 15, 24, 5, 25},
+		{385, 28, 15, 15, 10, 16},
+	},
+	{ // version 21
+		{932, 28, 4, 116, 4, 117},
+		{714, 26, 17, 42, 0, 0},
+		{512, 28, 17, 22, 6, 23},
+		{406, 30, 19, 16, 6, 17},
+	},
+	{ // version 22
+		{1006, 28, 2, 111, 7, 112},
+		{782, 28, 17, 46, 0, 0},
+		{568, 30, 7, 24, 16, 25},
+		{442, 24, 34, 13, 0, 0},
+	},
+	{ // version 23
+		{1094, 30, 4, 121, 5, 122},
+		{860, 28, 4, 47, 14, 48},
+		{614, 30, 11, 24, 14, 25},
+		{464, 30, 16, 15, 14, 16},
+	},
+	{ // version 24
+		{1174, 30, 6, 117, 4, 118},
+		{914, 28, 6, 45, 14, 46},
+		{664, 30, 11, 24, 16, 25},
+		{514, 30, 30, 16, 2, 17},
+	},
+	{ // version 25
+		{1276, 26, 8, 106, 4, 107},
+		{1000, 28, 8, 47, 13, 48},
+		{718, 30, 7, 24, 22, 25},
+		{538, 30, 22, 15, 13, 16},
+	},
+	{ // version 26
+		{1370, 28, 10, 114, 2, 115},
+		{1062, 28, 19, 46, 4, 47},
+		{754, 28, 28, 22, 6, 23},
+		{596, 30, 33, 16, 4, 17},
+	},
+	{ // version 27
+		{1468, 30, 8, 122, 4, 123},
+		{1128, 28, 22, 45, 3, 46},
+		{808, 30, 8, 23, 26, 24},
+		{628, 30, 12, 15, 28, 16},
+	},
+	{ // version 28
+		{1531, 30, 3, 117, 10, 118},
+		{1193, 28, 3, 45, 23, 46},
+		{871, 30, 4, 24, 31, 25},
+		{661, 30, 11, 15, 31, 16},
+	},
+	{ // version 29
+		{1631, 30, 7, 116, 7, 117},
+		{1267, 28, 21, 45, 7, 46},
+		{911, 30, 1, 23, 37, 24},
+		{701, 30, 19, 15, 26, 16},
+	},
+	{ // version 30
+		{1735, 30, 5, 115, 10, 116},
+		{1373, 28, 19, 47, 10, 48},
+		{985, 30, 15, 24, 25, 25},
+		{745, 30, 23, 15, 25, 16},
+	},
+	{ // version 31
+		{1843, 30, 13, 115, 3, 116},
+		{1455, 28, 2, 46, 29, 47},
+		{1033, 30, 42, 24, 1, 25},
+		{793, 30, 23, 15, 28, 16},
+	},
+	{ // version 32
+		{1955, 30, 17, 115, 0, 0},
+		{1541, 28, 10, 46, 23, 47},
+		{1115, 30, 10, 24, 35, 25},
+		{845, 30, 19, 15, 35, 16},
+	},
+	{ // version 33
+		{2071, 30, 17, 115, 1, 116},
+		{1631, 28, 14, 46, 21, 47},
+		{1171, 30, 29, 24, 19, 25},
+		{901, 30, 11, 15, 46, 16},
+	},
+	{ // version 34
+		{2191, 30, 13, 115, 6, 116},
+		{1725, 28, 14, 46, 23, 47},
+		{1231, 30, 44, 24, 7, 25},
+		{961, 30, 59, 16, 1, 17},
+	},
+	{ // version 35
+		{2306, 30, 12, 121, 7, 122},
+		{1812, 28, 12, 47, 26, 48},
+		{1286, 30, 39, 24, 14, 25},
+		{986, 30, 22, 15, 41, 16},
+	},
+	{ // version 36
+		{2434, 30, 6, 121, 14, 122},
+		{1914, 28, 6, 47, 34, 48},
+		{1354, 30, 46, 24, 10, 25},
+		{1054, 30, 2, 15, 64, 16},
+	},
+	{ // version 37
+		{2566, 30, 17, 122, 4, 123},
+		{1992, 28, 29, 46, 14, 47},
+		{1426, 30, 49, 24, 10, 25},
+		{1096, 30, 24, 15, 46, 16},
+	},
+	{ // version 38
+		{2702, 30, 4, 122, 18, 123},
+		{2102, 28, 13, 46, 32, 47},
+		{1502, 30, 48, 24, 14, 25},
+		{1142, 30, 42, 15, 32, 16},
+	},
+	{ // version 39
+		{2812, 30, 20, 117, 4, 118},
+		{2216, 28, 40, 47, 7, 48},
+		{1582, 30, 43, 24, 22, 25},
+		{1222, 30, 10, 15, 67, 16},
+	},
+	{ // version 40
+		{2956, 30, 19, 118, 6, 119},
+		{2334, 28, 18, 47, 31, 48},
+		{1666, 30, 34, 24, 34, 25},
+	},
+}
+
 // 检测是否不是数据块
 func (qrItem *QRCodeItem) checkNotIsData(x, y int) bool {
 	finderSize := 9
@@ -917,10 +1190,43 @@ func (qrItem *QRCodeItem) checkNotIsData(x, y int) bool {
 	}
 
 	// Alignment Patterns
+	qrDataInfo := QRDataInfoList[qrItem.version]
+	for _, alignPattern := range qrDataInfo.alignmentPatterns {
+		if (math.Abs(float64(alignPattern - x))) < 3 {
+			if (math.Abs(float64(alignPattern - y))) < 3 {
+				return true
+			}
+		}
+	}
 
 	return false
 }
 
-func (qrItem *QRCodeItem) readBit(x, y int) error {
-	return nil
+// 数据掩码
+func (qrItem *QRCodeItem) dataMask(i, j int) int {
+	k := 0
+	switch qrItem.mask {
+	case 0:
+		k = (i + j) % 2
+	case 1:
+		k = i % 2
+	case 2:
+		k = j % 3
+	case 3:
+		k = (i + j) % 3
+	case 4:
+		k = ((i / 2) + (j / 3)) % 2
+	case 5:
+		k = (i*j)%2 + (i*j)%3
+	case 6:
+		k = ((i*j)%2 + (i*j)%3) % 2
+	case 7:
+		k = ((i*j)%3 + (i+j)%2) % 2
+	default:
+		return 0
+	}
+	if k != 0 {
+		return 0
+	}
+	return 1
 }
